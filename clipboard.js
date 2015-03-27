109c27cda11ca80c44de03b6659879e2cd4b1c6b
Lucas Garron <code@garron.net>
2015-03-27 04:26:38 -0700

    Change all private variables to use underscores.

--- clipboard.js
+++ clipboard.js
@@ -1,34 +1,34 @@
 var clipboard = {};
 
 clipboard.copy = (function() {
-  var interceptCopy = false;
-  var copyObject; // Map from data type (e.g. "text/html") to value.
+  var _intercept = false;
+  var _data; // Map from data type (e.g. "text/html") to value.
 
   document.addEventListener("copy", function(e){
-    if (interceptCopy) {
-      for (key in copyObject) {
-        e.clipboardData.setData(key, copyObject[key]);
+    if (_intercept) {
+      for (key in _data) {
+        e.clipboardData.setData(key, _data[key]);
       }
       e.preventDefault();
     }
   });
 
   return function(data) {
-    interceptCopy = true;
-    copyObject = (typeof data === "string" ? {"text/plain": data} : data);
+    _intercept = true;
+    _data = (typeof data === "string" ? {"text/plain": data} : data);
     document.execCommand("copy");
-    interceptCopy = false;
+    _intercept = false;
   };
 }());
 
 clipboard.paste = (function() {
-  var interceptPaste = false;
+  var _intercept = false;
   var _resolve;
   var _dataType;
 
   document.addEventListener("paste", function(e) {
-    if (interceptPaste) {
-      interceptPaste = false;
+    if (_intercept) {
+      _intercept = false;
       e.preventDefault();
       _resolve(e.clipboardData.getData(_dataType));
     }
@@ -36,7 +36,7 @@ clipboard.paste = (function() {
 
   return function(dataType) {
     return new Promise(function(resolve, reject) {
-      interceptPaste = true; // Race condition?
+      _intercept = true; // Race condition?
       _resolve = resolve;
       _dataType = dataType || "text/plain";
       document.execCommand("paste");
