58d927f073568c432525cad7f2a2ab8f2c2d0122
Lucas Garron <code@garron.net>
2015-10-14 13:04:36 -0700

    Fix pull request and tweak formatting.

--- clipboard.js
+++ clipboard.js
@@ -1,9 +1,9 @@
-//  Import support http://stackoverflow.com/questions/13673346/supporting-both-commonjs-and-amd
+//  Import support https://stackoverflow.com/questions/13673346/supporting-both-commonjs-and-amd
 (function(name, definition) {
-    if (typeof module != 'undefined') module.exports = definition();
-    else if (typeof define == 'function' && typeof define.amd == 'object') define(definition);
+    if (typeof module != "undefined") module.exports = definition();
+    else if (typeof define == "function" && typeof define.amd == "object") define(definition);
     else this[name] = definition();
-}('mod', function() {
+}("clipboard", function() {
 
   var clipboard = {};
 
