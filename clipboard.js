1903aa0ee5f412a5cda39eab55054347e4260c05
Lucas Garron <code@garron.net>
2017-02-11 14:41:30 -0800

    Fall back to bogusSelect() only if the regular selection didn't work. Fixes #38 in Chrome.

--- clipboard.js
+++ clipboard.js
@@ -69,21 +69,30 @@
         } else {
           _data = data;
         }
-        try {
-          bogusSelect();
-          if (document.execCommand("copy")) {
-            // document.execCommand is synchronous: http://www.w3.org/TR/2015/WD-clipboard-apis-20150421/#integration-with-rich-text-editing-apis
-            // So we can call resolve() back here.
+
+        function triggerCopy(tryBogusSelect) {
+          try {
+            if (document.execCommand("copy")) {
+              // document.execCommand is synchronous: http://www.w3.org/TR/2015/WD-clipboard-apis-20150421/#integration-with-rich-text-editing-apis
+              // So we can call resolve() back here.
+              cleanup();
+              resolve();
+            }
+            else {
+              if (!tryBogusSelect) {
+                bogusSelect();
+                triggerCopy(true);
+              } else {
+                throw new Error("Unable to copy. Perhaps it's not available in your browser?");
+              }
+            }
+          } catch (e) {
             cleanup();
-            resolve();
+            reject(e);
           }
-          else {
-            throw new Error("Unable to copy. Perhaps it's not available in your browser?");
-          }
-        } catch (e) {
-          cleanup();
-          reject(e);
         }
+        triggerCopy(false);
+
       });
     };
   })();
