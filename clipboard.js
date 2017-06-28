a38029d627c1f4219ad364745cc3f6bb18b24c37
Lucas Garron <code@garron.net>
2017-06-27 20:59:43 -0700

    Reject copy data that isn't the right format. Closes #43.

--- clipboard.js
+++ clipboard.js
@@ -67,8 +67,10 @@
           _data = {"text/plain": data};
         } else if (data instanceof Node) {
           _data = {"text/html": new XMLSerializer().serializeToString(data)};
-        } else {
+        } else if (_data instanceof Object){
           _data = data;
+        } else {
+          reject("Invalid data type. Must be string, DOM node, or an object mapping MIME types to strings.")
         }
 
         function triggerCopy(tryBogusSelect) {
