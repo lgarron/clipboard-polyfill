b40e68bd10ac6823baf4cf1026cc85d3d38929b8
Lucas Garron <code@garron.net>
2017-10-26 02:15:19 -0700

    Update build and update version to 2.3.0.
    Changes:
    - suppressMissingPlainTextWarning() has been replaced by supressWarnings(), which now also suppresses warnings for unknown data types in clipboard.DT.setData().

--- build/DT.d.ts
+++ build/DT.d.ts
@@ -1,4 +1,5 @@
-export default class DT {
+export declare function suppressDTWarnings(): void;
+export declare class DT {
     private m;
     setData(type: string, value: string): void;
     getData(type: string): string | undefined;
--- build/clipboard-polyfill.d.ts
+++ build/clipboard-polyfill.d.ts
@@ -1,9 +1,9 @@
 import { Promise } from "es6-promise";
-import DT from "./DT";
+import { DT } from "./DT";
 export default class ClipboardPolyfill {
     static readonly DT: typeof DT;
     static setDebugLog(f: (s: string) => void): void;
-    static suppressMissingPlainTextWarning(): void;
+    static suppressWarnings(): void;
     static write(data: DT): Promise<void>;
     static writeText(s: string): Promise<void>;
     static read(): Promise<DT>;
