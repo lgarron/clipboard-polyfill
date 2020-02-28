f37334aca9c43a7be2a755878a7c614e45fbdc6e
Lucas Garron <code@garron.net>
2020-02-27 21:01:38 -0800

    Store `IEWindow` in var.

--- src/internet-explorer.ts
+++ src/internet-explorer.ts
@@ -7,21 +7,23 @@ interface IEWindow extends Window {
   };
 }
 
+const ieWindow = window as unknown as IEWindow;
+
 export function seemToBeInIE(): boolean {
   return typeof ClipboardEvent === "undefined" &&
-         typeof (window as IEWindow).clipboardData !== "undefined" &&
-         typeof (window as IEWindow).clipboardData.setData !== "undefined";
+    typeof ieWindow.clipboardData !== "undefined" &&
+    typeof ieWindow.clipboardData.setData !== "undefined";
 }
 
 export function writeTextIE(text: string): boolean {
   // IE supports text or URL, but not HTML: https://msdn.microsoft.com/en-us/library/ms536744(v=vs.85).aspx
   // TODO: Write URLs to `text/uri-list`? https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/Recommended_drag_types
-  return (window as IEWindow).clipboardData.setData("Text", text);
+  return ieWindow.clipboardData.setData("Text", text);
 }
 
 // Returns "" if the read failed, e.g. because the user rejected the permission.
 export async function readTextIE(): Promise<string> {
-  const text = (window as IEWindow).clipboardData.getData("Text");
+  const text = ieWindow.clipboardData.getData("Text");
   if (text === "") {
     throw new Error("Empty clipboard or could not read plain text from clipboard");
   }
