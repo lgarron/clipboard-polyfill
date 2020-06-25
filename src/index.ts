825a44a033c21988cda44f16444cf979b502bf1b
Lucas Garron <code@garron.net>
2020-06-25 04:34:30 -0700

    Export spec types.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 export { write, writeText, read, readText } from "./clipboard-polyfill";
 export { setDebugLog, suppressWarnings } from "./debug";
 export { ClipboardItemPolyfill as ClipboardItem } from "./ClipboardItem/ClipboardItemPolyfill";
+export type {
+  ClipboardItems,
+  ClipboardItemData,
+  ClipboardItemConstructor,
+  ClipboardItemDataType,
+  ClipboardItemDelayedCallback,
+  ClipboardItemInterface,
+  PresentationStyle,
+  ClipboardItemOptions,
+} from "./ClipboardItem/spec";
