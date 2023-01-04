76ca416353bff3284fddc962dade921114c0b720
Lucas Garron <code@garron.net>
2023-01-04 00:32:55 -0800

    Convert `const` and `let`.

--- src/clipboard-polyfill/promiseRecordMap.ts
+++ src/clipboard-polyfill/promiseRecordMap.ts
@@ -2,15 +2,15 @@ export function promiseRecordMap<T>(
   keys: string[],
   f: (key: string) => Promise<T>,
 ): Promise<Record<string, T>> {
-  const promiseList: Promise<T>[] = [];
-  for (const key of keys) {
+  var promiseList: Promise<T>[] = [];
+  for (var key of keys) {
     promiseList.push(f(key));
   }
   return Promise.all(promiseList).then(function (
     vList: T[],
   ): Record<string, T> {
-    const dataOut: Record<string, T> = {};
-    for (let i = 0; i < keys.length; i++) {
+    var dataOut: Record<string, T> = {};
+    for (var i = 0; i < keys.length; i++) {
       dataOut[keys[i]] = vList[i];
     }
     return dataOut;
