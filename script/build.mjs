c6ca74d307b9017371b6afa4891d340aca1b2dab
Lucas Garron <code@garron.net>
2023-01-04 02:18:34 -0800

    Update set of builds.

--- script/build.mjs
+++ script/build.mjs
@@ -1,50 +1,61 @@
 import { build } from "esbuild";
 
-await build({
-  entryPoints: ["src/clipboard-polyfill/targets/main.ts"],
+const es6 = {
   format: "esm",
   target: "es6",
   bundle: true,
-  outfile: "dist/main/clipboard-polyfill.main.es6.js",
+};
+
+await build({
+  entryPoints: ["src/clipboard-polyfill/targets/main.ts"],
+  ...es6,
+  outfile: "dist/es6/main/clipboard-polyfill.main.es6.js",
 });
 
 await build({
   entryPoints: ["src/clipboard-polyfill/targets/text.ts"],
-  format: "esm",
-  target: "es6",
-  bundle: true,
-  outfile: "dist/text/clipboard-polyfill.text.es6.js",
-});
-
-await build({
-  entryPoints: ["src/clipboard-polyfill/targets/global-var.ts"],
-  target: "es5",
-  bundle: true,
-  outfile: "dist/global-var/clipboard-polyfill.global-var.es5.js",
-  banner: { js: '"use strict";' },
-});
-
-await build({
-  entryPoints: ["src/clipboard-polyfill/targets/global-var.promise.ts"],
-  target: "es5",
-  bundle: true,
-  outfile: "dist/global-var/clipboard-polyfill.global-var.promise.es5.js",
-  banner: { js: '"use strict";' },
+  ...es6,
+  outfile: "dist/es6/text/clipboard-polyfill.text.es6.js",
 });
 
 await build({
   entryPoints: ["src/clipboard-polyfill/targets/overwrite-globals.ts"],
-  target: "es5",
-  bundle: true,
-  outfile: "dist/overwrite-globals/clipboard-polyfill.overwrite-globals.es5.js",
-  banner: { js: '"use strict";' },
+  ...es6,
+  outfile:
+    "dist/es6/overwrite-globals/clipboard-polyfill.overwrite-globals.es6.js",
 });
 
-await build({
-  entryPoints: ["src/clipboard-polyfill/targets/overwrite-globals.promise.ts"],
-  target: "es5",
-  bundle: true,
-  outfile:
-    "dist/overwrite-globals/clipboard-polyfill.overwrite-globals.promise.es5.js",
-  banner: { js: '"use strict";' },
-});
+async function buildES5(src, targetStem) {
+  const common = {
+    entryPoints: [src],
+    target: "es5",
+    bundle: true,
+    banner: { js: '"use strict";' },
+  };
+  // await build({
+  //   ...common,
+  //   outfile: `${targetStem}.es5.js`,
+  // });
+  await build({
+    ...common,
+    minify: true,
+    outfile: `${targetStem}.min.es5.js`,
+  });
+}
+
+buildES5(
+  "src/clipboard-polyfill/targets/global-var.ts",
+  "dist/es5/global-var/clipboard-polyfill.global-var",
+);
+buildES5(
+  "src/clipboard-polyfill/targets/global-var.promise.ts",
+  "dist/es5/global-var/clipboard-polyfill.global-var.promise",
+);
+buildES5(
+  "src/clipboard-polyfill/targets/overwrite-globals.ts",
+  "dist/es5/overwrite-globals/clipboard-polyfill.overwrite-globals",
+);
+buildES5(
+  "src/clipboard-polyfill/targets/overwrite-globals.promise.ts",
+  "dist/es5/overwrite-globals/clipboard-polyfill.overwrite-globals.promise",
+);
