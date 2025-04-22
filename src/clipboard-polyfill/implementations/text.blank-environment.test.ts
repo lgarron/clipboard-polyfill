a58fa026de060e34a65a081635107f943a9661ed
Lucas Garron <code@garron.net>
2025-04-22 07:54:58 -0700

    [text.blank-environment.test.ts] Test against the error constructor instead of the message.
    `bun` changed its message. The constructor is more stable, and specific enough for out test case.

--- src/clipboard-polyfill/implementations/text.blank-environment.test.ts
+++ src/clipboard-polyfill/implementations/text.blank-environment.test.ts
@@ -7,8 +7,6 @@ setDebugLog(consoleLogMock);
 test("writeText(…) failure in a blank environmnent", async () => {
   const { writeText } = await import("./text");
 
-  expect(async () => writeText("hello")).toThrowError(
-    "Can't find variable: document",
-  );
+  expect(async () => writeText("hello")).toThrowError(ReferenceError);
   expect(consoleLogMock).toHaveBeenCalledTimes(0);
 });
